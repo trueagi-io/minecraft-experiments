4a3bda449451a7069d1ce560206fd22afd0abc04
Innokenty <innokenty@singularitynet.io>
2023-07-17 10:42:01 +0300

    test_water updated

--- vision_tests/test_water.py
+++ vision_tests/test_water.py
@@ -2,15 +2,7 @@ import cv2
 from utils.common import *
 import numpy
 import time
-from mcdemoaux.vision.neural import get_image
 from tagilmo.utils.vereya_wrapper import MCConnector, RobustObserverWithCallbacks
-import tagilmo.utils.mission_builder as mb
-
-
-SCALE = 2 
-WIDTH = 320 * SCALE
-HEIGHT = 240 * SCALE
-
 
 def detect_water_see(image):
      blue_lower = numpy.array([111.35, 163.3 , 158.7 ])
@@ -18,62 +10,29 @@ def detect_water_see(image):
      see_low = numpy.array([106, 155, 234], dtype=numpy.uint8)
      see_high = numpy.array([126, 175, 254], dtype=numpy.uint8)
  
-     hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
      mask = cv2.inRange(hsv, blue_lower, blue_upper)
      mask_see = cv2.inRange(hsv, see_low, see_high)
      return mask, mask_see
 
 
 def main():
-    mc, rob = start_mission()
-    mc.safeStart()
-    time.sleep(1)
+    mc = MCConnector.connect(name='Cristina', video=True)
+    rob = RobustObserverWithCallbacks(mc)
     while True:
         rob.updateAllObservations()
         time.sleep(0.15)
-        frame = rob.getCachedObserve('getImageFrame')
-        image = get_image(frame, 1, SCALE)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = rob.getCachedObserve('getImageFrame')
+        if image is not None:
+            image = image.pixels
+        else:
+            continue
 
         mask, mask_see = detect_water_see(image)
         cv2.imshow('img', image)
         cv2.imshow('water', mask)
         cv2.imshow('see', mask_see)
         cv2.waitKey(300)
-
-
-def start_mission():
-    miss = mb.MissionXML()
-    colourmap_producer = mb.ColourMapProducer(width=WIDTH, height=HEIGHT)
-    video_producer = mb.VideoProducer(width=WIDTH, height=HEIGHT, want_depth=False)
-    colourmap_producer = None
-
-    obs = mb.Observations()
-    agent_handlers = mb.AgentHandlers(observations=obs)
-
-    agent_handlers = mb.AgentHandlers(observations=obs,
-        colourmap_producer=colourmap_producer,
-        video_producer=video_producer)
-
-    miss = mb.MissionXML(agentSections=[mb.AgentSection(name='Cristina',
-             agenthandlers=agent_handlers)])
-    
-    # seed 28 - with a see
-    world = mb.defaultworld(
-        seed='28',
-        forceReset="false")
-
-    
-    miss.setWorld(world)
-    miss.serverSection.initial_conditions.allowedmobs = "Pig Sheep Cow Chicken Ozelot Rabbit Villager"
-    # disable passage of time:
-    miss.serverSection.initial_conditions.time_pass = 'false'
-    miss.serverSection.initial_conditions.time_start = "1000"
-
-    mc = MCConnector(miss)
-    obs1 = RobustObserverWithCallbacks(mc)
-    return mc, obs1
-
 
 if __name__ == '__main__':
    main()
