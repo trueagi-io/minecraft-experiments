ccfffb616230264d6fff75f2abedb7abe7aa5608
CICS-Oleg <scherbakovolegdk@yandex.ru>
2025-12-03 12:09:16 +0300

    minor fix

--- vla/benchmarks/block_type_classification/dataset_index_creation_example.py
+++ vla/benchmarks/block_type_classification/dataset_index_creation_example.py
@@ -1,14 +1,16 @@
 from manager import DatasetManager
 from config import LabelType, PATH_TO_RAW_DATA
 
-manager = DatasetManager(
-    directory=PATH_TO_RAW_DATA,
-    label_type=LabelType.DISTANCE,
-    dataset_size=200,
-    train_split=0.25,
-    num_bins=50
-)
 
-train_file, test_file, t = manager.create("los_dataset")
+if __name__ == "__main__":
+    manager = DatasetManager(
+        directory=PATH_TO_RAW_DATA,
+        label_type=LabelType.DISTANCE,
+        dataset_size=200,
+        train_split=0.25,
+        num_bins=50
+    )
 
-print("Generation time:", t)
+    train_file, test_file, t = manager.create("los_dataset")
+
+    print("Generation time:", t)
